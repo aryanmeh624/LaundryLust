7ab3bbe8430bf80e9740df3e4c674c246c6aa76f
SoggyBiscuit28 <soggybiscuit28@gmail.com>
2024-09-02 22:32:12 +0530

    database files updated again and again and again

--- lib/flashcard.dart
+++ lib/flashcard.dart
@@ -1,35 +1,15 @@
-// flashcard.dart2
-/*
-id
-pic
-lastworn
-dirty
-name
- */
 import 'dart:typed_data';
+import 'cloth_data/main_DB.dart' as pog;
 import 'cloth_data/get_clothes.dart';
-class Clothboii {
-  final String cloth_name;
-  final int last_worn;
-  final int dirty;
-  final Uint8List pic;
-  final int id;
-  Clothboii({
-    required this.cloth_name,
-    required this.last_worn,
-    required this.dirty,
-    required this.pic,
-    required this.id,
-});
-}
+
 Future<void> _loadData() async {
   try {
-    List<Clothboii> _flashcards = [];
+    List<pog.laundryData> _flashcards = [];
     var laundryDataList = await laundryGet(); // Get the list of dictionaries
     _flashcards = laundryDataList.map((data) {
-      return Clothboii(
-          cloth_name: data.name,
-          last_worn: data.lastWorn,
+      return pog.laundryData(
+          name: data.name,
+          lastWorn: data.lastWorn,
           dirty: data.dirty,
           id: data.id,
           pic: data.pic,
@@ -39,3 +19,18 @@ Future<void> _loadData() async {
     // Handle the error as needed
   }
 }
+
+// class Clothboii {
+//   final String cloth_name;
+//   final int last_worn;
+//   final int dirty;
+//   final Uint8List pic;
+//   final int id;
+//   Clothboii({
+//     required this.cloth_name,
+//     required this.last_worn,
+//     required this.dirty,
+//     required this.pic,
+//     required this.id,
+// });
+// }
